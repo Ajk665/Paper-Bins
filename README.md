26a478d975727443f930e751361d420ed1fa198d
Ajk665 <72042993+Ajk665@users.noreply.github.com>
2020-10-19 15:33:51 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Paper-Bins
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground;
+var ball;
+var bin1,bin2,bin3;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(400,575,800,90);
+	ball = new Ball(200,565,20,20);
+	bin1 = new Bin(575,565,100,20);
+	bin2 = new Bin(635,515,20,100);
+	bin3 = new Bin(515,515,20,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  drawSprites();
+  ball.display();
+  bin1.display();
+ bin2.display();
+ bin3.display();
+ ifPressed
+}
+
+
+function ifPressed(){
+	if(keyDown("rightArrow")){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x=85, y=85});
+		Matter.Body.setStatic(ball,false);
+	}
+	}
